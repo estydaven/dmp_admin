3c0f13512d870393762ed294bc8b219637392a6e
estydaven <esetnod1111@gmail.com>
2022-08-24 13:42:49 +0300

    fix some edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,39 @@
 // Sidebar menu tabs
 
-$(document).on("click", ".menu-nav__item", function() {
-	let numberIndex = $(this).index();
+$(document).on("click", ".menu-nav__item", function () {
+    let numberIndex = $(this).index();
     $(".templates-new").removeClass("active");
     $(".templates__wrapper").removeClass("hidden");
     $(".users__inner").removeClass("hide");
     $(".administrate").removeClass("show");
 
-	if (!$(this).is("active")) {
-		$(".menu-nav__item").removeClass("active");
-		$(".content-list__item").removeClass("active");
+    if (!$(this).is("active")) {
+        $(".menu-nav__item").removeClass("active");
+        $(".content-list__item").removeClass("active");
 
-		$(this).addClass("active");
-		$(".content-list").find(".content-list__item:eq(" + numberIndex + ")").addClass("active");
+        $(this).addClass("active");
+        $(".content-list").find(".content-list__item:eq(" + numberIndex + ")").addClass("active");
 
-		let listItemHeight = $(".content-list")
-			.find(".content-list__item:eq(" + numberIndex + ")")
-			.innerHeight();
-		$(".content-list").height(listItemHeight + "px");
-	}
+        let listItemHeight = $(".content-list")
+            .find(".content-list__item:eq(" + numberIndex + ")")
+            .innerHeight();
+        $(".content-list").height(listItemHeight + "px");
+    }
 });
 
 // Mobile menu
 
-$('.burger').on('click', function() {
+$('.burger').on('click', function () {
     $('.sidebar-menu__inner').toggleClass('open');
     $('.burger__line_last').toggleClass('burger__line_mob');
 });
-$('.menu-nav__item').on('click', function() {
+$('.menu-nav__item').on('click', function () {
     $('.sidebar-menu__inner').toggleClass('open');
     $('.burger__line_last').removeClass('burger__line_mob');
 });
 // Show/hide menu 
 
-$('.menu-collapse').on('click', function() {
+$('.menu-collapse').on('click', function () {
     $(this).removeClass('show');
     $(this).addClass('hide');
     $('.menu-collapsed').removeClass('hide');
@@ -46,7 +46,7 @@ $('.menu-collapse').on('click', function() {
     $('.sidebar-menu__logo').attr('src', 'images/icons/logo-small.svg').addClass('sidebar-menu__logo_collapse');
 });
 
-$('.menu-collapsed').on('click', function() {
+$('.menu-collapsed').on('click', function () {
     $(this).removeClass('show');
     $(this).addClass('hide');
     $('.menu-collapse').removeClass('hide');
@@ -61,43 +61,43 @@ $('.menu-collapsed').on('click', function() {
 
 // Show/hide new template block
 
-$('.header-template__button').on('click', function() {
-    if($(this).hasClass('header-template__button-template')) {
+$('.header-template__button').on('click', function () {
+    if ($(this).hasClass('header-template__button-template')) {
         $('.templates-new_template').addClass('active');
         $('.templates__wrapper_template').addClass('hidden');
     }
-    if($(this).hasClass('header-template__button-company')) {
+    if ($(this).hasClass('header-template__button-company')) {
         $('.templates-new_company').addClass('active');
         $('.templates__wrapper_company').addClass('hidden');
     }
 });
-$('.edits__item_edit').on('click', function() {
-    if($(this).hasClass('edits__item_template')) {
-        $('.templates-new_template').addClass('active');
+$('.edits__item_edit').on('click', function () {
+    if ($(this).hasClass('edits__item_template')) {
+        $('.templates-new_edit').addClass('active');
         $('.templates__wrapper_template').addClass('hidden');
     }
-    if($(this).hasClass('edits__item_company')) {
+    if ($(this).hasClass('edits__item_company')) {
         $('.templates-new_company').addClass('active');
         $('.templates__wrapper_company').addClass('hidden');
     }
 });
-$('.button_save').on('click', function() {
-    if($(this).hasClass('button_template')) {
+$('.button_save').on('click', function () {
+    if ($(this).hasClass('button_template')) {
         $('.templates-new_template').removeClass('active');
         $('.templates__wrapper_template').removeClass('hidden');
     }
-    if($(this).hasClass('button_company')) {
+    if ($(this).hasClass('button_company')) {
         $('.templates-new_company').removeClass('active');
         $('.templates__wrapper_company').removeClass('hidden');
         $('.niche').removeClass('show');
     }
 });
-$('.button-reset').on('click', function() {
-    if($(this).hasClass('button-reset_template')) {
+$('.button-reset').on('click', function () {
+    if ($(this).hasClass('button-reset_template')) {
         $('.templates-new_template').addClass('active');
         $('.templates__wrapper_template').addClass('hidden');
     }
-    if($(this).hasClass('button-reset_company')) {
+    if ($(this).hasClass('button-reset_company')) {
         $('.templates-new_company').addClass('active');
         $('.templates__wrapper_company').addClass('hidden');
     }
@@ -106,10 +106,10 @@ $('.button-reset').on('click', function() {
 // Custom date field
 
 $(".rangeDate").flatpickr({
-	emableTime: true,
+    emableTime: true,
     mode: 'range',
     dateFormat: "d.m.y",
-	minDate: "01.01.22",
+    minDate: "01.01.22",
     defaultDate: ["21.06.2022", "25.06.2022"],
     conjunction: " . ",
     "locale": "ru"
@@ -118,28 +118,28 @@ $(".rangeDate").flatpickr({
 $(".template-form__input_date").flatpickr({
     "locale": "ru",
     dateFormat: "d.m.y",
-	minDate: "01.01.22",
+    minDate: "01.01.22",
     altInputClass: 'class',
 });
 
 // Custom file field
 
-(function (document, window, index){
+(function (document, window, index) {
     'use strict';
     let inputs = document.querySelectorAll('.inputfile');
     let spanInput = document.querySelector('.upload-name');
-	let textUpload = document.querySelector('.upload-text');
+    let textUpload = document.querySelector('.upload-text');
     Array.prototype.forEach.call(inputs, function (input) {
         let label = input.nextElementSibling,
-                labelVal = label.innerHTML;        
+            labelVal = label.innerHTML;
         input.addEventListener('change', function (e) {
             let fileName = '';
             //if (spanInput.innerText == fileName) {
-                spanInput.classList.add('upload-name_active');
-                textUpload.style.display = 'none';                
+            spanInput.classList.add('upload-name_active');
+            textUpload.style.display = 'none';
             //}
             if (this.files && this.files.length > 1)
-                fileName = ( this.getAttribute('data-multiple-caption') || '' ).replace('{count}', this.files.length);
+                fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
             else
                 fileName = e.target.value.split('\\').pop();
 
@@ -148,7 +148,7 @@ $(".template-form__input_date").flatpickr({
             else
                 label.innerHTML = labelVal;
         });
-        
+
         // Firefox bug fix
         input.addEventListener('focus', function () {
             input.classList.add('has-focus');
@@ -164,7 +164,7 @@ $(".template-form__input_date").flatpickr({
 const dropBtn = document.querySelectorAll('.edits_token');
 
 dropBtn.forEach((el) => {
-    el.onclick = function() {
+    el.onclick = function () {
         let dropdown = this.parentNode.getElementsByClassName('token-info')[0];
         dropdown.classList.toggle('show');
         let button = this.getElementsByClassName('edits__item_info')[0];
@@ -174,7 +174,7 @@ dropBtn.forEach((el) => {
 
 if (window.innerWidth < 769) {
     dropBtn.forEach((el) => {
-        el.onclick = function() {
+        el.onclick = function () {
             let dropdown = this.parentNode.getElementsByClassName('token-info')[0];
             dropdown.classList.toggle('show');
             let button = this.getElementsByClassName('edits__item_info')[0];
@@ -219,7 +219,7 @@ jQuery(($) => {
         $(this).parent().prev().find('.select__text').text($(this).text());
         if ($(this).parent().hasClass("select__list_niche")) {
             $(this).parent().prev().text($(this).text());
-        }        
+        }
         $(this).parent().prev().prev().val($(this).text());
     });
 
@@ -232,7 +232,7 @@ jQuery(($) => {
 
     $(".select").on("click", ".select__item_dropdown", function () {
         $(".niche").addClass("show");
-    });        
+    });
 });
 
 // Show niche block and change count of templates
@@ -244,38 +244,38 @@ let checkCount = 0;
 checkboxes.forEach((el) => {
     el.addEventListener('change', function () {
         var check = event.target;
-        
+
         if (check.checked === true) {
-            checkCount += 1;           
+            checkCount += 1;
         }
         if (check.checked !== true) {
-            checkCount -= 1;                       
+            checkCount -= 1;
         }
-        countField.value = checkCount; 
+        countField.value = checkCount;
     });
 });
 
 // Reducing text length in mobile block 'Niche'
 if (($(window).width() < 769)) {
     let sizeWords = 12,
-    wordsContent= $('.mob-text'),
-    newText = wordsContent.text();
-    
-    if(newText.length > sizeWords){
+        wordsContent = $('.mob-text'),
+        newText = wordsContent.text();
+
+    if (newText.length > sizeWords) {
         wordsContent.text(newText.slice(0, sizeWords) + ' ...');
     }
 
     let sizeWordsDescr = 18,
-    wordsContentDescr= $('.mob-descr'),
-    newTextDescr = wordsContentDescr.text();
-    
-    if(newTextDescr.length > sizeWordsDescr){
+        wordsContentDescr = $('.mob-descr'),
+        newTextDescr = wordsContentDescr.text();
+
+    if (newTextDescr.length > sizeWordsDescr) {
         wordsContentDescr.text(newTextDescr.slice(0, sizeWordsDescr) + ' ...');
     }
 }
 
 // Show/hide administrate block
- 
+
 const adminButtons = document.querySelectorAll('.edits__item_admin');
 const adminBlock = document.querySelector('.administrate');
 const usersBlock = document.querySelector('.users__inner');
